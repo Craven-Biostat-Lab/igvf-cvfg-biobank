38c978f542a05e1c9e57f5d405523bb13d66d95a
Yuriy Sverchkov <sverchkov@wisc.edu>
2025-03-19 10:11:38 -0500

    Restructured as python package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='igvf-cvfg-biobank',
+    version='0.1',
+    description='Coding Variants Focus Group Biobank Validation anlyses',
+    url='https://github.com/Craven-Biostat-Lab/igvf-cvfg-biobank',
+    author='Yuriy Sverchkov',
+    author_email='sverchkov@wisc.edu',
+    license='MIT',
+    packages=['cvfgaou'],
+    zip_safe=False
+)
