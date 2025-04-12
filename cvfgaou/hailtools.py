d78462f98928141883a5a27a15024b0ff269a6a3
Yuriy Sverchkov <sverchkov@wisc.edu>
2025-04-11 20:58:53 -0500

    Refactoring for alternate variant selection function

--- cvfgaou/hailtools.py
+++ cvfgaou/hailtools.py
@@ -2,20 +2,15 @@
 
 import hail as hl
 
-def get_cols_with_variants(
-    variant_df, wgs_mt,
-    contig_col='Chromosome',
-    pos_col='Genomic_coordinates',
-    ref_col='Ref_allele',
-    alt_col='Alt_allele',
-    reference_genome='GRCh38'
+def get_filtered_mt(
+    variant_df, wgs_mt, *
+    contig_col,
+    pos_col,
+    ref_col,
+    alt_col,
+    reference_genome
 ):
-    """Get the set of MatrixTable columns that have calls
-    
-    Given a dataframe describing variants and a MatrixTable with variant rows
-    and sample columns (i.e. like the AoU WGS MT), output the columns that have
-    the specified variants as a dataframe.
-    """
+    """Helper used in get_cols_with_variants and get_variant_sets_mt_row"""
 
     # Performance is much improved when the WGS table is first filtered to
     # intervals.
@@ -45,9 +40,62 @@ def get_cols_with_variants(
     variant_ht = variant_ht.key_by(variant_ht.locus, variant_ht.alleles)
 
     # Filter down wgs_mt
-    filtered_mt = hl.filter_intervals(wgs_mt, intervals).semi_join_rows(variant_ht)
+    return hl.filter_intervals(wgs_mt, intervals).semi_join_rows(variant_ht)
+
+
+def get_cols_with_variants(
+    variant_df, wgs_mt,
+    contig_col='Chromosome',
+    pos_col='Genomic_coordinates',
+    ref_col='Ref_allele',
+    alt_col='Alt_allele',
+    reference_genome='GRCh38'
+):
+    """Get the set of MatrixTable columns that have calls
+    
+    Given a dataframe describing variants and a MatrixTable with variant rows
+    and sample columns (i.e. like the AoU WGS MT), output the columns that have
+    the specified variants as a dataframe.
+    """
 
     # Get person table
-    return filtered_mt.filter_cols(
+    return get_filtered_mt(
+        variant_df=variant_df, wgs_mt=wgs_mt,
+        contig_col=contig_col,
+        pos_col=pos_col,
+        ref_col=ref_col,
+        alt_col=alt_col,
+        reference_genome=reference_genome
+    ).filter_cols(
         hl.agg.any(filtered_mt.GT.is_non_ref())
     ).cols().to_pandas()
+
+
+def get_variant_sets_mt_row(
+    label_dict, variant_df, wgs_mt, *,
+    contig_col,
+    pos_col,
+    ref_col,
+    alt_col,
+    reference_genome='GRCh38'
+):
+    
+    # Filter down wgs_mt
+    filtered_mt = get_filtered_mt(
+        variant_df=variant_df, wgs_mt=wgs_mt,
+        contig_col=contig_col,
+        pos_col=pos_col,
+        ref_col=ref_col,
+        alt_col=alt_col,
+        reference_genome=reference_genome
+    ).annotate_rows(select=True)
+
+    return (
+        filtered_mt.
+        group_rows_by('select').
+        aggregate_entries(exposure=hl.agg.any(filtered_mt.GT.is_non_ref())).
+        result().
+        annotate_rows(**label_dict).
+        key_rows_by(*label_dict.keys())
+    )
+ 
