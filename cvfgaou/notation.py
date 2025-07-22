87bc30076705996f45e1a00115108f1f97bbf714
Yuriy Sverchkov <sverchkov@wisc.edu>
2025-07-22 15:57:25 -0500

    Added notation helper for frames

--- cvfgaou/notation.py
+++ cvfgaou/notation.py
@@ -4,3 +4,16 @@
 def var_str(contig, pos, ref, alt):
     """Compose the variant ID string contig:pos:ref>alt"""
     return f'{contig}:{pos}:{ref}>{alt}'
+
+
+def var_col(df, contig_col, pos_col, ref_col, alt_col):
+    """Create a variant ID column given contig, pos, ref, alt columns in a dataframe"""
+    return df.apply(
+        lambda row: var_str(
+            row[contig_col],
+            row[pos_col],
+            row[ref_col],
+            row[alt_col]
+        ),
+        axis='columns'
+    )
