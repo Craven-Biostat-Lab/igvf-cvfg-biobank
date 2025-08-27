62f94f7402380a96927d3607c39b8b09d9e7e911
Yuriy Sverchkov <sverchkov@wisc.edu>
2025-08-27 17:13:51 -0500

    Added strengh/direction of evidence helper tables in notation module

--- cvfgaou/notation.py
+++ cvfgaou/notation.py
@@ -1,8 +1,28 @@
 """Helper constants and functions to maintain consistent entity notation."""
 
+import pandas as pd
+
 # Unicode character codes
 GEQ_CHAR = '\u2265'
 LEQ_CHAR = '\u2264'
+
+# Strength of evidence naming/reference table
+# Stores correspondence of point magnitude to strength phrase
+SOE_TABLE = (
+    (8, 'very strong'),
+    (4, 'strong'),
+    (3, 'moderate+'),
+    (2, 'modetate'),
+    (1, 'supporting')
+)
+
+# Direction of evidence naming/reference table
+# Stores directional information:
+# inequality, sign, criterion, label, comparator, reverse_comparator
+DOE_TABLE = (
+    (GEQ_CHAR, '+', 'PP3', 'Pathogenic', pd.Series.ge, pd.Series.le),
+    (LEQ_CHAR, '-', 'BP4', 'Benign', pd.Series.le, pd.Series.ge)
+)
 
 def var_str(contig, pos, ref, alt):
     """Compose the variant ID string contig:pos:ref>alt"""
